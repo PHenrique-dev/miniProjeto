ace5608bc2956107be8599d664d94d06231661cf
PHenrique-dev <pedrinho03s4mini@gmail.com>
2024-04-18 20:59:00 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Estacionamento = require('./estacionamento')
 const Veiculo = require('./meu-projeto/atividade/veiculo')
 
 const veiculo = new Estacionamento();
-veiculo.adiciona(new Veiculo('Carro', 'QJPD34', 2, 20));
+veiculo.adiciona(new Veiculo('Carro', 'QJPD34', 2));
 
 veiculo.adicionaTaxa(10);
 veiculo.calculaTotal();
